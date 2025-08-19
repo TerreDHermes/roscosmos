e1741b91a9c233b728fe28d99e1088680afad0be
unknown <vikden.99@mail.ru>
2025-08-19 16:50:30 +0300

    [orderService] update: main.go - create_order, pay_order, get_order

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -15,6 +16,7 @@ import (
 	orderV1 "github.com/TerreDHermes/roscosmos/shared/pkg/openapi/order/v1"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 const (
@@ -24,17 +26,59 @@ const (
 	shutdownTimeout   = 10 * time.Second
 )
 
+var ErrOrderNotFound = errors.New("order not found")
+
 // OrderStorage представляет потокобезопасное хранилище данных о заказах
 type OrderStorage struct {
 	mu     sync.RWMutex
 	orders map[string]*orderV1.OrderDto
 }
 
-// NewOrderStorage создает новое хранилище данных о погоде
+// NewOrderStorage создает новое хранилище данных о заказах
 func NewOrderStorage() *OrderStorage {
 	return &OrderStorage{
 		orders: make(map[string]*orderV1.OrderDto),
 	}
+}
+
+func (ost *OrderStorage) CreateOrderInStorage(orderUUID string, totalPrice float32, userUUID string, partUUIDs []string) (*orderV1.OrderDto, error) {
+	ost.mu.Lock()
+	defer ost.mu.Unlock()
+
+	ost.orders[orderUUID] = &orderV1.OrderDto{}
+	order_status := orderV1.NewOptOrderStatus(orderV1.OrderStatusPENDINGPAYMENT)
+
+	ost.orders[orderUUID].SetOrderUUID(orderUUID)
+	ost.orders[orderUUID].SetUserUUID(userUUID)
+	ost.orders[orderUUID].SetPartUuids(partUUIDs)
+	ost.orders[orderUUID].SetTotalPrice(totalPrice)
+	ost.orders[orderUUID].SetStatus(order_status)
+
+	ost.orders[orderUUID].PaymentMethod.SetTo("")
+
+	return ost.orders[orderUUID], nil
+}
+
+func (ost *OrderStorage) GetOrderFromStorage(orderUUID string) (*orderV1.OrderDto, error) {
+	ost.mu.RLock()
+	defer ost.mu.RUnlock()
+	if _, ok := ost.orders[orderUUID]; !ok {
+		return nil, ErrOrderNotFound
+	}
+	return ost.orders[orderUUID], nil
+}
+
+// статус → `PAID`, сохраняет `transaction_uuid`, `payment_method`.
+func (ost *OrderStorage) SetPayOrderInformation(orderUUID string, status orderV1.OrderStatus, transactionUUID string, paymentMethod orderV1.PaymentMethod) error {
+	ost.mu.RLock()
+	defer ost.mu.RUnlock()
+	paymentMethodOpt := orderV1.NewOptPaymentMethod(paymentMethod)
+	orderStatusOpt := orderV1.NewOptOrderStatus(status)
+
+	ost.orders[orderUUID].SetStatus(orderStatusOpt)
+	ost.orders[orderUUID].SetTransactionUUID(transactionUUID)
+	ost.orders[orderUUID].SetPaymentMethod(paymentMethodOpt)
+	return nil
 }
 
 // OrderHandler реализует интерфейс orderV1.Handler для обработки запросов к API заказов
@@ -49,24 +93,94 @@ func NewOrderHandler(storage *OrderStorage) *OrderHandler {
 	}
 }
 
+// Получаем детали через `InventoryService.ListParts` и считаем сумму (total_price)
 func (h *OrderHandler) CreateOrder(ctx context.Context, req *orderV1.CreateOrderRequest) (orderV1.CreateOrderRes, error) {
-	return nil, nil
+	orderUUID := uuid.New()
+	totalPrice := 100.00
+
+	orderDto, _ := h.storage.CreateOrderInStorage(orderUUID.String(), float32(totalPrice), req.UserUUID, req.PartUuids)
+
+	return &orderV1.CreateOrderResponse{
+		OrderUUID:  orderDto.GetOrderUUID(),
+		TotalPrice: orderDto.GetTotalPrice(),
+	}, nil
 }
 
+// - Ищет заказ по UUID. Если найден — возвращает. Если не найден — 404 Not Found.
 func (h *OrderHandler) GetOrder(ctx context.Context, params orderV1.GetOrderParams) (orderV1.GetOrderRes, error) {
-	return nil, nil
+	orderDto, err := h.storage.GetOrderFromStorage(params.OrderUUID)
+	if err != nil {
+		if errors.Is(err, ErrOrderNotFound) {
+			return &orderV1.NotFoundError{Message: "Order not found"}, nil
+		}
+		return nil, err
+	}
+
+	return &orderV1.GetOrderResponse{
+		OrderDto: *orderDto,
+	}, nil
+}
+
+// - Находит заказ по `order_uuid`. Если не существует — возвращает 404 Not Found.
+// - Вызывает `PaymentService.PayOrder`, передаёт `user_uuid`, `order_uuid` и `payment_method`. Получает`transaction_uuid`.
+// - Обновляет заказ: статус → `PAID`, сохраняет `transaction_uuid`, `payment_method`.
+func (h *OrderHandler) PayOrder(ctx context.Context, req *orderV1.PayOrderRequest, params orderV1.PayOrderParams) (orderV1.PayOrderRes, error) {
+	orderDto, err := h.storage.GetOrderFromStorage(params.OrderUUID)
+	if err != nil {
+		if errors.Is(err, ErrOrderNotFound) {
+			return &orderV1.NotFoundError{Message: "Order not found"}, nil
+		}
+		return nil, err
+	}
+
+	err = ValidateStatus(string(orderDto.GetStatus().Value))
+	if err != nil {
+		return &orderV1.BadRequestError{
+			Message: err.Error(),
+		}, nil
+	}
+
+	paymentMethod, err := ValidatePaymentMethod(req.GetPaymentMethod())
+	if err != nil {
+		return &orderV1.BadRequestError{
+			Message: err.Error(),
+		}, nil
+	}
+
+	transactionUUID := uuid.New()
+
+	h.storage.SetPayOrderInformation(orderDto.OrderUUID, orderV1.OrderStatusPAID, transactionUUID.String(), paymentMethod)
+
+	return &orderV1.PayOrderResponse{
+		TransactionUUID: transactionUUID.String(),
+	}, nil
 }
 
 func (h *OrderHandler) OrderCancel(ctx context.Context, params orderV1.OrderCancelParams) (orderV1.OrderCancelRes, error) {
 	return nil, nil
 }
 
-func (h *OrderHandler) PayOrder(ctx context.Context, req *orderV1.PayOrderRequest, params orderV1.PayOrderParams) (orderV1.PayOrderRes, error) {
-	return nil, nil
-}
-
 func (h *OrderHandler) NewError(ctx context.Context, err error) *orderV1.GenericErrorStatusCode {
 	return &orderV1.GenericErrorStatusCode{}
+}
+
+func ValidatePaymentMethod(pm string) (orderV1.PaymentMethod, error) {
+	paymentMethod := orderV1.PaymentMethod(pm)
+	switch paymentMethod {
+	case orderV1.PaymentMethodCARD, orderV1.PaymentMethodSBP, orderV1.PaymentMethodCREDITCARD, orderV1.PaymentMethodINVESTORMONEY, orderV1.PaymentMethodUNKNOWN:
+		return paymentMethod, nil
+	default:
+		return "", fmt.Errorf("invalid payment method: %s", pm)
+	}
+}
+
+func ValidateStatus(status string) error {
+	switch status {
+	case string(orderV1.OrderStatusPENDINGPAYMENT):
+		return nil
+	default:
+		return fmt.Errorf("invalid status: %s", status)
+	}
 }
 
 func main() {
